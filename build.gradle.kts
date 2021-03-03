71b1265e60be20431252c29f8033aaf33cfa5328
Dmytro Shestakov <altais@ukr.net>
2021-03-03 03:59:48 +0200

    Add Link and Unlink Gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.nio.file.Files.createSymbolicLink
+import java.nio.file.Files.deleteIfExists
+
+import java.nio.file.Path.of as withPath
 
 plugins {
     kotlin("jvm") version "1.4.31"
     application
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    languageVersion = "1.4"
 }
 
 group = "me.dmytro"
@@ -16,15 +25,14 @@ dependencies {
     implementation("org.json:json:20201115")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
-}
-
 application {
     mainClass.set("MainKt")
 }
 
 tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
     withType<Jar> {
         manifest {
             attributes["Main-Class"] = application.mainClass
@@ -39,6 +47,23 @@ tasks {
                     it.appendBytes(archiveFile.get().asFile.readBytes())
                     it.setExecutable(true)
                 }
+        }
+    }
+
+    register("Link") {
+        dependsOn(withType<Jar>())
+        doLast {
+            val homeDir = System.getenv("HOME");
+            createSymbolicLink(
+                withPath("${homeDir}/.local/bin/${project.name}"),
+                withPath("$buildDir/libs/${project.name}")
+            )
+        }
+    }
+    register("Unlink") {
+        doLast {
+            val homeDir = System.getenv("HOME");
+            deleteIfExists(withPath("${homeDir}/.local/bin/${project.name}"))
         }
     }
 }
